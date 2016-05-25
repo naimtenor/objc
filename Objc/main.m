f80c015daac287aa6b1c89eafaf66b1b02fd01fa
naimtenor <naimtenor@gmail.com>
2016-05-25 12:13:36 +0900

    6.1

--- Objc/main.m
+++ Objc/main.m
@@ -13,18 +13,16 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Calculator *deskCalc = [[Calculator alloc] init];
+        int number;
         
-        [deskCalc setAccumulator:100.0];
-        [deskCalc add:200.];
-        [deskCalc divide:15.0];
-        [deskCalc substract:10.0];
-        [deskCalc multiply:5];
-        NSLog(@"The result is %g", [deskCalc accumulator]);
+        NSLog(@"Type in your number: ");
+        scanf("%i", &number);
         
+        if (number < 0) {
+            number = -number;
+        }
         
-        
-        [deskCalc release];
+        NSLog(@"The absolute value is %i", number);
     }
     return 0;
 }
